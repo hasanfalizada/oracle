46f7c47159bc7be47a3f40f5400c45cf784163cf
Hasan Alizada <ha@hasanalizada.net>
2018-08-26 16:08:56 +0400

    DDL locks has been added

--- view_locks.sql
+++ view_locks.sql
@@ -17,3 +17,12 @@ SELECT T.STATUS,
        JOIN V$SESSION S ON T.SES_ADDR = S.SADDR
        JOIN V$LOCKED_OBJECT LO ON LO.XIDSQN = T.XIDSQN
        JOIN DBA_OBJECTS DO ON DO.OBJECT_ID = LO.OBJECT_ID;
+
+  SELECT NVL (owner, 'SYS') owner,
+         session_id,
+         name,
+         TYPE,
+         mode_held,
+         mode_requested
+    FROM dba_ddl_locks
+ORDER BY 1, 2, 3;
