19e1ac9bac148027f8965c8425b195d61c795992
Hasan Alizada <ha@hasanalizada.net>
2018-10-05 11:44:34 +0400

    Defining and killing OS process has been added

--- view_locks.sql
+++ view_locks.sql
@@ -26,3 +26,18 @@ SELECT T.STATUS,
          mode_requested
     FROM dba_ddl_locks
 ORDER BY 1, 2, 3;
+
+-- Defining and killing OS process
+
+SELECT * FROM dba_blockers;
+
+SELECT s.sid,
+       s.serial#,
+       s.username,
+       s.osuser,
+       p.spid,
+       s.machine,
+       p.terminal,
+       s.program
+  FROM v$session s, v$process p
+ WHERE s.paddr = p.addr;
