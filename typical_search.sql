b589cb29299dca0d085da5d604fb08c502efb9a7
Hasan Alizada <ha@hasanalizada.net>
2019-01-29 11:25:04 +0400

    !

--- typical_search.sql
+++ typical_search.sql
@@ -2,4 +2,5 @@ SELECT name,
   FROM table t
  WHERE NVL (name, ' ') = NVL (NULL, NVL (name, ' '))
    AND NVL (surname, ' ') = NVL (NULL, NVL (surname, ' '));
-   AND NVL (age, 0) = NVL (NULL, NVL (age, 0))
+   AND NVL (age, 0) = NVL (NULL, NVL (age, 0)) -- In case of number datatype
+   AND NVL (cbcc.main, ' ') = CASE WHEN NULL IS NULL THEN ' ' ELSE 'Y' END -- In case of nullable columns
