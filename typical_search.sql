a7939a7064b24c812d04943b00d6366d9755376a
Hasan Alizada <ha@hasanalizada.net>
2019-01-29 11:01:41 +0400

    !

--- typical_search.sql
+++ typical_search.sql
@@ -1,3 +1,4 @@
 SELECT name,
   FROM table t
- WHERE NVL (name, ' ') = NVL (NULL, NVL (name, ' '));
+ WHERE NVL (name, ' ') = NVL (NULL, NVL (name, ' '))
+    OR NVL (surname, ' ') = NVL (NULL, NVL (surname, ' '));
